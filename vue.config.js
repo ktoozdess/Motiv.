a406117c5f104171fa4af2f6bac378c6ad6371dc
Ktoozdess <tigerfpvteam@gmail.com>
2023-01-10 15:46:00 +0500

    pwa color

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: "Motiv.",
+    themeColor: "#8409F9"
+  }
 })
